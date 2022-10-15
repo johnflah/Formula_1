2f30ec79529861949d164555d5b769bc9682353b
John O'Flaherty <johnflah@github.com>
2022-10-15 14:50:06 +0100

    Initial app setup

---
+++ src/test/java/com/jof/formula/FormulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jof.formula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
